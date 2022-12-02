5a3c152fdf32847149ef3f4c5fa36317af20961d
Alberto Perez <113957252+OkSoBasicly@users.noreply.github.com>
2022-12-01 20:40:52 -0600

    Add files via upload

---
+++ Database/sandbox-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (284842949, 1, 'Hi')
+
+--UPDATE sandbox SET double_value = -2.112135 WHERE id = 116;
+
+--DELETE FROM sandbox WHERE id = 155;
+
+SELECT * FROM sandbox;
